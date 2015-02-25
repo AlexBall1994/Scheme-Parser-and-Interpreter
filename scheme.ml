b98bfdaebf6ca9286a7d8191f77ee97b21c564e1
alexball1994 <aball9423@comcast.net>
2015-02-24 19:38:47 -0500

    new stuff

---
+++ scheme.ml
@@ -0,0 +1,12 @@
+(* CMSC 330 Project - Scheme Parser & Interpreter *)
+ 
+(* NAME:  Alexander Ball*)
+ 
+(* To load this file into the OCaml interpreter, type
+ 
+   #use "scheme.ml"
+ 
+   at the OCaml prompt
+*)
+ 
+
